d96d32259c40ff1edd97a05e8da0644a16550aad
estudiante <estudiante@ISIS2603-341.sis.virtual.uniandes.edu.co>
2018-10-21 21:55:14 -0500

    Medico y medico-list componentes agregados al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import {PacienteModule} from './paciente/paciente.module';
 
 import { AppComponent } from './app.component';
+import { MedicoComponent } from './medico/medico.component';
+import { MedicoListComponent } from './medico/medico-list/medico-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MedicoComponent,
+    MedicoListComponent
   ],
   imports: [
     BrowserModule,
