c1c73bdd485c61bc269e0722825ae93b9932f74f
mitooos <35747350+mitooos@users.noreply.github.com>
2018-10-22 21:01:32 -0500

    Revert "Revert "M.hoyos""

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 's4-front-MedicinaPrepagada';
+  title: string;
+  
+  ngOnInit() : void {
+      this.title = "Medisistemas";
+  }
+  
+  constructor() {}
 }
