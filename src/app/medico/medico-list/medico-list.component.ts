7e699a33398d81a0d143c411614d14b2fabd3ad9
estudiante <estudiante@ISIS2603-341.sis.virtual.uniandes.edu.co>
2018-10-23 06:50:25 -0500

    Lista de medicos desplegada sin conexión al back

--- src/app/medico/medico-list/medico-list.component.ts
+++ src/app/medico/medico-list/medico-list.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Medico} from './medico.model';
+
 @Component({
   selector: 'app-medico-list',
   templateUrl: './medico-list.component.html',
   styleUrls: ['./medico-list.component.css']
 })
 export class MedicoListComponent implements OnInit {
-
-  constructor() { }
-
+  medicos: Medico[];
+  constructor() {
+    this.medicos = [];
+    var medico = new Medico('David', 4234234, 'david@gmail.com', 123456789);
+    this.medicos.push(medico);
+  }
+  
   ngOnInit() {
   }
 
