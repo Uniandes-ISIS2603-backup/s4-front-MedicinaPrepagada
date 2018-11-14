b3ef5103a7b55d963894e4ebaf8c30829bc434d7
estudiante <estudiante@ISIS2603-319.sis.virtual.uniandes.edu.co>
2018-11-14 18:02:20 -0500

    Se muestra consultorio como sub recurso de sede

---
+++ src/app/sede/sede-detail.ts
@@ -0,0 +1,12 @@
+
+import {Sede} from './sede';
+import {Consultorio} from './consultorio';
+
+export class SedeDetail extends Sede
+{
+    consultorios: Consultorio[];
+    
+    image: string;
+}
+
+
