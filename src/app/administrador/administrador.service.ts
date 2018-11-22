6f18837b1ff44b12676010027a4470a093a810ae
estudiante <estudiante@ISIS2603-322.sis.virtual.uniandes.edu.co>
2018-11-22 11:03:27 -0500

    sirve el update de administrador

--- src/app/administrador/administrador.service.ts
+++ src/app/administrador/administrador.service.ts
@@ -13,7 +13,6 @@ import {Administrador} from './administrador';
 
 const API_URL = environment.apirURL;
 const administradores = '/administradores';
-const administradorDetail = '/administradorDetails';
 import 'rxjs/add/operator/catch';
 
 @Injectable()
@@ -32,19 +31,19 @@ export class AdministradorService
     
     getAdministrador(administradorId): Observable<Administrador>
     {
-        return this.http.get<Administrador>(API_URL + administradores+ '/' + administradorId).catch(err => this.handleError(err));
+        return this.http.get<Administrador>(API_URL + administradores + '/' + administradorId).catch(err => this.handleError(err));
     }
     
     createAdministrador(administrador): Observable<Administrador> {
         return this.http.post<Administrador>(API_URL + administradores, administrador).catch(err => this.handleError(err));
     }
     
-    deleteAdministrador(administradorId): Observable<Boolean> {
-        return this.http.delete<Boolean>(API_URL + administradores + '/' + administradorId);
+    deleteAdministrador(administradorId): Observable<boolean> {
+        return this.http.delete<boolean>(API_URL + administradores + '/' + administradorId);
     }
     
-    updateAdministrador(administrador): Observable<Boolean> {
-        return this.http.put<Boolean>(API_URL + administradores , administrador).catch(err => this.handleError(err));;
+    updateAdministrador(administrador, administradorId ): Observable<Administrador> {
+        return this.http.put<Administrador>(API_URL + administradores + '/' + administradorId  , administrador).catch(err => this.handleError(err));;
     }
     
     private handleError(error: any)
