91b8d6b24b3f4e22cea8321f6b2d168b6890de04
Simon <estudiante@ISIS2603-319.sis.virtual.uniandes.edu.co>
2018-11-28 23:48:49 -0500

    Se agrego docuemtacion de metodos

--- src/app/horario-atencion/horarioAtencion.Service.ts
+++ src/app/horario-atencion/horarioAtencion.Service.ts
@@ -20,26 +20,35 @@ export class HorarioAtencionService{
     
     constructor (private http: HttpClient) {}
     
+    /**
+     * Retorna los horarios de atencion
+     */
     getHorariosAtencion() : Observable<HorarioAtencion[]>{
         return this.http.get<HorarioAtencion[]> (API_URL + horariosAtencion);
     }
     
-      
+     /**
+      * Retorna un horario de atencion
+      */ 
     getHorarioAtencion(horarioAtencionId): Observable<HorarioAtencion>
     {
         return this.http.get<HorarioAtencion>(API_URL + horariosAtencion+ '/' + horarioAtencionId ).catch(err => this.handleError(err));
     }
-    
+    /**
+     * Crea un horario de atencion
+     */
     createHorarioAtencion(horarioAtencion): Observable<HorarioAtencion>{
         return this.http.post<HorarioAtencion>(API_URL + horariosAtencion, horarioAtencion).catch(err => this.handleError(err));
     }
-    
+    /**
+     * Actualiza un horario de atencion
+     */
     updateHorarioAtencion(horarioAtencion): Observable<HorarioAtencion>{
         return this.http.put<HorarioAtencion>(API_URL + horariosAtencion + '/' + horarioAtencion.id , horarioAtencion).catch(err => this.handleError(err));
     }
 
          /**
-    * Deletes a sede
+    * Deletes a horario de atencion
     * @param sedeId The sede's id
     * @returns True if the sede was deleted, false otherwise
     */
