d92c20930ee31c813388822b69891abff5739b11
estudiante <estudiante@ISIS2603-323.sis.virtual.uniandes.edu.co>
2018-11-28 21:53:40 -0500

    Modulo para crear una cita medica desde el paciente

---
+++ src/app/paciente/paciente-cita-medica-create/paciente-cita-medica-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paciente-cita-medica-create',
+  templateUrl: './paciente-cita-medica-create.component.html',
+  styleUrls: ['./paciente-cita-medica-create.component.css']
+})
+export class PacienteCitaMedicaCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
